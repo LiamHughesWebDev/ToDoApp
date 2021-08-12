4c828bedd89a57358df21e5679d102719160f49c
Liam Hughes <ljhughes599@gmail.com>
2021-08-12 01:30:38 -0400

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAVCpcHJBjmTCEYMNm_e73cxILj1l858LE",
+    authDomain: "to-do-app-6185b.firebaseapp.com",
+    projectId: "to-do-app-6185b",
+    storageBucket: "to-do-app-6185b.appspot.com",
+    messagingSenderId: "257059510083",
+    appId: "1:257059510083:web:f9e45f5d5aeedec40ac1f6",
+    measurementId: "G-3L1BL0VCXK"
+  }
 };
